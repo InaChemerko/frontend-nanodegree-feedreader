5213d83b25c29aa1fd2d24dea0eac9dcbf65a48c
Ina Chemerko <innachemerko@gmail.com>
2018-06-25 15:47:30 -0700

    menu is changed when the menu icon is clicked

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -72,17 +72,27 @@ $(function() {
             expect($('body').hasClass('menu-hidden')).toBe(true);
          });
 
-
-
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
 
+          it('menu is changed when the menu icon is clicked', function(){
+            var menuIcon = $('.menu-icon-link');
+
+            menuIcon.click();
+            expect($('body').hasClass('menu-hidden')).toBe(false);
+
+            menuIcon.click();
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+          });
+
     });
 
     /* TODO: Write a new test suite named "Initial Entries" */
+
+    describe('Initial Entries', function(){
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -90,6 +100,11 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+
+         it 
+
+
+     });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
 
